463076369d43464194bdc47afef3f574767e17cf
Killian de Buitleir <killian.debuitleir@gmail.com>
2021-02-03 19:02:05 +0100

    logo testting

--- src/index.js
+++ src/index.js
@@ -1,38 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
-import Form from "./components/form";
-import Loans from "./components/loans";
 
 import s from "./styles.module.sass";
 
 const App = () => {
-  const [loans, setLoans] = useState([]);
-  const [showForm, setShowForm] = useState(false);
-
-  useEffect(() => {
-    const json = window.localStorage.getItem("loans");
-    const parsed = JSON.parse(json) || [];
-    setLoans([...parsed]);
-  }, []);
-
-  const toggleForm = (evt) => {
-    evt.preventDefault();
-    setShowForm(!showForm);
+  const [active, setActive] = useState(false);
+  const toggleAnimation = () => {
+    setActive(!active);
+    console.log("active: ", active);
   };
-
+  const activeClass = active ? s.active : null;
   return (
-    <main className={s.app}>
-      <button className={s.btn} onClick={toggleForm}>
-        Add Loan
-      </button>
-      <Form
-        loans={loans}
-        active={showForm}
-        setLoans={setLoans}
-        toggleForm={toggleForm}
-      />
-      <Loans loans={loans} />
+    <main className={s.app} onClick={toggleAnimation}>
+      <svg viewBox="0 0 100 100" className={`${s.svg} ${activeClass}`}>
+        <circle
+          cx="50"
+          cy="50"
+          r="44"
+          fill="none"
+          stroke="#000000"
+          strokeWidth="3"
+          className={`${s.circle} ${activeClass}`}
+        />
+        <polyline
+          points="12,73 23,15 39,93"
+          fill="none"
+          stroke="#000000"
+          strokeWidth="3"
+          strokeMiterlimit="10"
+          className={`${s.poly} ${activeClass}`}
+        >
+          <animate
+            attributeName="points"
+            dur="1.3s"
+            from="0,0 0,0 0,0"
+            to="12,73 23,15 39,93"
+          />
+        </polyline>
+        <text
+          className={`${s.text} ${activeClass}`}
+          style={{
+            fontFamily: "Futura-Medium",
+            transform: "translate(34px, 59px)",
+            fontSize: "28px",
+          }}
+        >
+          <tspan className={s.tspan}>l</tspan>
+          <tspan className={s.tspan}>t</tspan>
+          <tspan className={s.tspan}>h</tspan>
+          <tspan className={s.tspan}>O</tspan>
+        </text>
+      </svg>
     </main>
   );
 };
