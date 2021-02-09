bf43856870b4ee29df8722d49d9530006047407c
Killian de Buitleir <killian.debuitleir@gmail.com>
2021-02-09 12:47:49 +0100

    udpates

--- src/index.js
+++ src/index.js
@@ -18,8 +18,6 @@ const App = () => {
         <rect width="100" height="100" mask="url(#hole)" className={s.rect} />
         <mask id="hole">
           <rect
-            x="0"
-            y="0"
             width="100"
             height="100"
             fill="white"
