4db47cad0fec732321c79a3713e8ab2163a6289d
Killian de Buitleir <killian.debuitleir@gmail.com>
2021-02-09 17:28:58 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import gsap from "gsap";
 import s from "./styles.module.sass";
 
 const App = () => {
-  const [active, setActive] = useState(true);
+  const [active, setActive] = useState(false);
   const toggleAnimation = () => {
     setActive(!active);
   };
@@ -14,6 +14,41 @@ const App = () => {
 
   return (
     <main className={s.app} onClick={toggleAnimation}>
+      <svg
+        viewBox="0 0 100 100"
+        className={clsx(s.svg, s.inverse, activeClass)}
+      >
+        <circle
+          cx="50"
+          cy="50"
+          r="44"
+          fill="none"
+          stroke="#000000"
+          strokeWidth="3"
+          className={clsx(s.circle, activeClass)}
+        />
+        <path
+          d="M 12,73 23,15 39,93"
+          stroke="#000000"
+          strokeWidth="3"
+          fill="none"
+          strokeMiterlimit="10"
+          className={clsx(s.path, activeClass)}
+        />
+        <text
+          className={clsx(s.text, activeClass)}
+          style={{
+            fontFamily: "Futura-Medium",
+            transform: "translate(34px, 59px)",
+            fontSize: "28px",
+          }}
+        >
+          <tspan className={s.tspan}>l</tspan>
+          <tspan className={s.tspan}>t</tspan>
+          <tspan className={s.tspan}>h</tspan>
+          <tspan className={s.tspan}>O</tspan>
+        </text>
+      </svg>
       <svg viewBox="0 0 100 100" className={clsx(s.svg, activeClass)}>
         <rect width="100" height="100" mask="url(#hole)" className={s.rect} />
         <mask id="hole">
